2315f618eae666f102394d0cb34a1c9239df0c80
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-02-28 17:01:30 +0100

    Add files via upload

---
+++ dotenv.hpp
@@ -0,0 +1,58 @@
+#ifndef DOTENV_HPP
+#define DOTENV_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string obtainEnv(std::string line, std::string env, char seperator);
+static void splitLine(std::string line, std::string& env, std::string& value, char seperator);
+
+std::string obtainEnv(std::string fileName, std::string env, char seperator = '=') {
+    if (env.size() == 1 && env[0] == seperator) {
+        throw std::runtime_error("Env can not equal seperator.");
+    }
+
+    std::string text;
+    std::string tempEnv;
+    std::string value;
+    std::ifstream envFile(fileName);
+
+    if (!envFile) {
+        throw std::runtime_error("Failed to open file: " + fileName);
+    }
+
+    while (std::getline (envFile, text)) {
+        splitLine(text, tempEnv, value, seperator);
+        if (tempEnv == env) {
+            return value;
+        }
+    }
+
+    return "";
+}
+
+static void splitLine(std::string line, std::string& env, std::string& value, char seperator) {
+    bool reachedSeperator = false;
+    env.clear();
+    value.clear();
+
+    for (int i = 0; i <= line.size(); i++)
+    {
+        if (!reachedSeperator) {
+            if (line[i] == seperator) {
+                reachedSeperator = true;
+            }
+            
+            else {
+                env += line[i];
+            }
+        }
+
+        else {
+            value += line[i];
+        }
+    }
+}
+
+#endif
